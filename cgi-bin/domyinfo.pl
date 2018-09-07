a3bfcb68ee3ce2b7e1670a8925a6076e161cd688
ZhiFeng Hu <hufeng1987@gmail.com>
2018-09-08 00:06:42 +0800

    Add strict and warning

--- cgi-bin/domyinfo.pl
+++ cgi-bin/domyinfo.pl
@@ -8,75 +8,82 @@
 #      论坛地址： http://bbs.LeoBBS.com/            #
 #####################################################
 
-    if ($avatars eq "on") {
-	if (($personalavatar)&&($personalwidth)&&($personalheight)) { #自定义头像存在
-	    $personalavatar =~ s/\$imagesurl/${imagesurl}/o;
-	    if (($personalavatar =~ /\.swf$/i)&&($flashavatar eq "yes")) {
-		$personalavatar=uri_escape($personalavatar);
-	        $useravatar = qq(<br><OBJECT CLASSID="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" WIDTH=$personalwidth HEIGHT=$personalheight><PARAM NAME=MOVIE VALUE=$personalavatar><PARAM NAME=PLAY VALUE=TRUE><PARAM NAME=LOOP VALUE=TRUE><PARAM NAME=QUALITY VALUE=HIGH><EMBED SRC=$personalavatar WIDTH=$personalwidth HEIGHT=$personalheight PLAY=TRUE LOOP=TRUE QUALITY=HIGH></EMBED></OBJECT>);
-	    }
-	    else {
-	        $personalavatar=uri_escape($personalavatar);
-		$useravatar = qq(<br><a href=profile.cgi?action=modify title=编辑您的个人资料><img src=$personalavatar width=$personalwidth height=$personalheight border=1></a>);
-	    }
-	}
-        elsif (($useravatar ne "noavatar") && ($useravatar)) {
-            $useravatar=uri_escape($useravatar);
-	    $useravatar = qq(<br><a href=profile.cgi?action=modify title=编辑您的个人资料><img src=$imagesurl/avatars/$useravatar.gif border=1></a>);
+use strict;
+use warnings;
+if ($avatars eq "on") {
+    if (($personalavatar) && ($personalwidth) && ($personalheight)) {
+        #自定义头像存在
+        $personalavatar =~ s/\$imagesurl/${imagesurl}/o;
+        if (($personalavatar =~ /\.swf$/i) && ($flashavatar eq "yes")) {
+            $personalavatar = uri_escape($personalavatar);
+            $useravatar = qq(<br><OBJECT CLASSID="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" WIDTH=$personalwidth HEIGHT=$personalheight><PARAM NAME=MOVIE VALUE=$personalavatar><PARAM NAME=PLAY VALUE=TRUE><PARAM NAME=LOOP VALUE=TRUE><PARAM NAME=QUALITY VALUE=HIGH><EMBED SRC=$personalavatar WIDTH=$personalwidth HEIGHT=$personalheight PLAY=TRUE LOOP=TRUE QUALITY=HIGH></EMBED></OBJECT>);
         }
-	elsif (($oicqnumber) && ($oicqnumber =~ /[0-9]/)) {
-	    $useravatar = qq~<a href=profile.cgi?action=modify title="您的 QQ 秀形象"><img src=http://qqshow-user.tencent.com/$oicqnumber/11/00/ width=70 height=113 border=1></a>~;
-	}
-        else { $useravatar=qq(<br><a href=profile.cgi?action=modify title=编辑您的个人资料><img src=$imagesurl/avatars/noavatar.gif border=1></a>); }
+        else {
+            $personalavatar = uri_escape($personalavatar);
+            $useravatar = qq(<br><a href=profile.cgi?action=modify title=编辑您的个人资料><img src=$personalavatar width=$personalwidth height=$personalheight border=1></a>);
+        }
     }
-    else { $useravatar=qq(<br><a href=profile.cgi?action=modify title=编辑您的个人资料><img src=$imagesurl/avatars/noavatar.gif border=1></a>); }
+    elsif (($useravatar ne "noavatar") && ($useravatar)) {
+        $useravatar = uri_escape($useravatar);
+        $useravatar = qq(<br><a href=profile.cgi?action=modify title=编辑您的个人资料><img src=$imagesurl/avatars/$useravatar.gif border=1></a>);
+    }
+    elsif (($oicqnumber) && ($oicqnumber =~ /[0-9]/)) {
+        $useravatar = qq~<a href=profile.cgi?action=modify title="您的 QQ 秀形象"><img src=http://qqshow-user.tencent.com/$oicqnumber/11/00/ width=70 height=113 border=1></a>~;
+    }
+    else {$useravatar = qq(<br><a href=profile.cgi?action=modify title=编辑您的个人资料><img src=$imagesurl/avatars/noavatar.gif border=1></a>);}
+}
+else {$useravatar = qq(<br><a href=profile.cgi?action=modify title=编辑您的个人资料><img src=$imagesurl/avatars/noavatar.gif border=1></a>);}
 
-    open (MSGOUT, "${lbdir}$msgdir/out/${memberfilename}_out.cgi");
-    sysread(MSGOUT, my $totalmessagesout,(stat(MSGOUT))[7]);
-    close (MSGOUT);
-    $totalmessagesout =~ s/\r//isg;
-    my @allmessages = split (/\n/,$totalmessagesout);
-    $totalmessagesout = @allmessages;
+open(MSGOUT, "${lbdir}$msgdir/out/${memberfilename}_out.cgi");
+sysread(MSGOUT, my $totalmessagesout, (stat(MSGOUT))[7]);
+close(MSGOUT);
+$totalmessagesout =~ s/\r//isg;
+my @allmessages = split(/\n/, $totalmessagesout);
+$totalmessagesout = @allmessages;
 
-    if ($unread eq "0") { $inboximg = qq~<img src=$imagesurl/images/inboxnonew.gif width=17>~; } else { $inboximg = qq~<img src=$imagesurl/images/inboxnew.gif width=17>~; }
+if ($unread eq "0") {$inboximg = qq~<img src=$imagesurl/images/inboxnonew.gif width=17>~;}
+else {$inboximg = qq~<img src=$imagesurl/images/inboxnew.gif width=17>~;}
 
-    require "${lbdir}data/cityinfo.cgi";
-    $mymoney = $numberofposts * $addmoney + $numberofreplys * $replymoney + $visitno * $loginmoney + $mymoney - $postdel * $delmoney + $jhcount * $addjhhb;
+require "${lbdir}data/cityinfo.cgi";
+$mymoney = $numberofposts * $addmoney + $numberofreplys * $replymoney + $visitno * $loginmoney + $mymoney - $postdel * $delmoney + $jhcount * $addjhhb;
 
-    $mymoney = "<font title=$mymoney>>999999999</font>" if ($mymoney > 999999999 );
-    $moneyname ="雷傲元" if ($moneyname eq "");
+$mymoney = "<font title=$mymoney>>999999999</font>" if ($mymoney > 999999999);
+$moneyname = "雷傲元" if ($moneyname eq "");
 
-    my ($mystatus, $mysaves, $mysavetime, $myloan, $myloantime, $myloanrating, $bankadd1, $bankadd2, $bankadd3, $bankadd4, $bankadd5) = split(/,/, $ebankdata);
-    unless ($mystatus eq "1" || $mystatus eq "-1" || $ebankdata eq "") {
-	$mysaves = "<B>没开户</B>";
-	$myloan = "<B>没贷款</B>";
+my ($mystatus, $mysaves, $mysavetime, $myloan, $myloantime, $myloanrating, $bankadd1, $bankadd2, $bankadd3, $bankadd4, $bankadd5) = split(/,/, $ebankdata);
+unless ($mystatus eq "1" || $mystatus eq "-1" || $ebankdata eq "") {
+    $mysaves = "<B>没开户</B>";
+    $myloan = "<B>没贷款</B>";
+}
+else {
+    if ($mystatus) {
+        $mysaves = "<B>$mysaves</B> $moneyname";
+        if ($myloan) {
+            $myloan = "<B>$myloan</B> $moneyname";
+        }
+        else {
+            $myloan = "<B>没贷款</B>";
+        }
     }
     else {
-        if ($mystatus) {
-	    $mysaves = "<B>$mysaves</B> $moneyname";
-	    if ($myloan) {
-	        $myloan = "<B>$myloan</B> $moneyname";
-	    } else {
-	        $myloan = "<B>没贷款</B>";
-	    }
-        } else {
-	    $mysaves = "<B>没开户</B>";
-	    $myloan = "<B>没贷款</B>";
-        }
+        $mysaves = "<B>没开户</B>";
+        $myloan = "<B>没贷款</B>";
     }
+}
 
-    $modscard_link = qq~\n :: [ <span style=cursor:hand onClick="javascript:openScript('modscard.cgi',420,320)"><font color=$fonthighlight><B>版主签到</B></font></span> ]~ if($membercode eq "ad" || $membercode eq "smo" || $membercode eq "cmo" || $membercode eq "mo" || $membercode eq "amo");
+$modscard_link = qq~\n :: [ <span style=cursor:hand onClick="javascript:openScript('modscard.cgi',420,320)"><font color=$fonthighlight><B>版主签到</B></font></span> ]~ if ($membercode eq "ad" || $membercode eq "smo" || $membercode eq "cmo" || $membercode eq "mo" || $membercode eq "amo");
 
-    $mysaves = "<font title=$mysaves>>999999999</font>" if ($mysaves > 999999999 );
-    $myloan = "<font title=$myloan>>999999999</font>" if ($myloan > 999999999 );
-    $postdel = 0 if ($postdel eq "");
-    
-    my $lasttimes = $lastgone + ($timedifferencevalue + $timezone)*3600;
-    $lasttimes = &dateformat($lasttimes);
+$mysaves = "<font title=$mysaves>>999999999</font>" if ($mysaves > 999999999);
+$myloan = "<font title=$myloan>>999999999</font>" if ($myloan > 999999999);
+$postdel = 0 if ($postdel eq "");
 
-    my $inmembernames = uri_escape($inmembername);
-    if (($maxmsgno eq "")||($membercode eq "ad")||($membercode eq "smo")||($membercode eq "cmo")||($membercode eq "amo")||($membercode eq "mo")) { $maxmsgno = qq~<B>没有限制</B>~; } else { $maxmsgno = qq~<font color=$fonthighlight><B>$maxmsgno</B></font> 条~; }
-    my $myoutput = qq~<tr><td bgcolor=\$titlecolor colspan=4 \$catbackpic><font color=\$titlefontcolor><B>-=>  个人状态</B>  [ 来自：$trueipaddress，$fromwhere1 。系统：$osinfo，$browseinfo。最后访问时间：$lasttimes ]</td></tr>
+my $lasttimes = $lastgone + ($timedifferencevalue + $timezone) * 3600;
+$lasttimes = &dateformat($lasttimes);
+
+my $inmembernames = uri_escape($inmembername);
+if (($maxmsgno eq "") || ($membercode eq "ad") || ($membercode eq "smo") || ($membercode eq "cmo") || ($membercode eq "amo") || ($membercode eq "mo")) {$maxmsgno = qq~<B>没有限制</B>~;}
+else {$maxmsgno = qq~<font color=$fonthighlight><B>$maxmsgno</B></font> 条~;}
+my $myoutput = qq~<tr><td bgcolor=\$titlecolor colspan=4 \$catbackpic><font color=\$titlefontcolor><B>-=>  个人状态</B>  [ 来自：$trueipaddress，$fromwhere1 。系统：$osinfo，$browseinfo。最后访问时间：$lasttimes ]</td></tr>
 <tr><td bgcolor=\$forumcolorone align=center width=18>$inboximg</td>
 <td bgcolor=\$forumcolortwo width=130 align=center>$useravatar</td>
 <td bgcolor=\$forumcolortwo width=*><img src=$imagesurl/images/none.gif width=340 height=1><BR>
@@ -102,7 +109,7 @@
 [ <a href=fav.cgi?action=show&member=$inmembernames>我的收藏夹</a> ] :: [ <span style=cursor:hand onClick="javascript:openScript('recopr.cgi?action=post',420,320)">我被回复的主题</span> ] :: [ <span style=cursor:hand onClick="javascript:openScript('recopr.cgi?action=reply',420,320)">我参与的主题</span> ] :: [ <span style=cursor:hand onClick="javascript:openScript('recopr.cgi?action=new',420,320)">论坛最新帖</span> ]$modscard_link</center>
 </td>~;
 if ($forumcached eq "yes") {
-    open (FILE, ">${lbdir}cache/myinfo/$memberfilename.pl");
+    open(FILE, ">${lbdir}cache/myinfo/$memberfilename.pl");
     $myoutput =~ s/\\/\\\\/isg;
     $myoutput =~ s/~/\\\~/isg;
     $myoutput =~ s/\$/\\\$/isg;
@@ -115,7 +122,7 @@ if ($forumcached eq "yes") {
     $myoutput =~ s/\\\@/\@/isg;
     $myoutput =~ s/\\\\/\\/isg;
     print FILE "1;\n";
-    close (FILE);
+    close(FILE);
 }
 $myoutput =~ s/\$titlecolor/$titlecolor/isg;
 $myoutput =~ s/\$catbackpic/$catbackpic/isg;
--- cgi-bin/doonlinepop.pl
+++ cgi-bin/doonlinepop.pl
@@ -8,57 +8,50 @@
 #      论坛地址： http://bbs.LeoBBS.com/            #
 #####################################################
 
-		if ($firstcome ne 'yes')
-		{
-			if (@onlinefr)
-			{
-				my $onlinefriend = join('　　', @onlinefr);
-				$onlinepopup = "你下面这些好友刚刚上线了！<p>$onlinefriend";
-				require 'onlinepop.pl';
-			}
-			$onlineprompt = uri_escape($onlineprompt);
-			$output .= qq~<script>var exp = new Date(); exp.setTime(exp.getTime() + ($membergone*60*1000)); document.cookie="onlineprompt=$onlineprompt; expires=" + exp.toGMTString();</script>~;
-		}
-		else
-		{
-			my $temptempuser = $tempusername;
-			$temptempuser =~ s/ /\_/g;
-			$temptempuser =~ tr/A-Z/a-z/;
-			$temptempuser =~ s/[\a\f\n\e\0\r\t\`\~\!\@\#\$\%\^\&\*\(\)\+\=\\\|\{\}\;\'\:\"\,\.\/\<\>\?]//isg;
-			if (open(FRD, "${lbdir}memfriend/$temptempuser.cgi"))
-			{
-				sysread(FRD, my $friendlist, (stat(FRD))[7]);
-				close(FRD);
-				$friendlist =~ s/\r//isg;
-				my @friendlist = split($/, $friendlist);
-				my @noonlinefr = ();
-				my @onlinefr = ();
-				foreach my $friend (@friendlist)
-				{
-					$friend =~ s/^＊＃！＆＊//;
-					$friend =~ s/[\r\n]*$//;
-					next if ($friend eq '');
-					if (grep(/^$friend\t/i, @onlinedata) > 0)
-					{
-						push(@onlinefr, $friend);
-					}
-					else
-					{
-						push(@noonlinefr, $friend);
-					}
-				}
-				if (@onlinefr)
-				{
-					my $onlinefriend = join('　　', @onlinefr);
-					$onlinepopup = "你下面这些好友正在线上！<p>$onlinefriend";
-					require 'onlinepop.pl';
-				}
-				if (@noonlinefr)
-				{
-					my $onlineprompt = join(',', @noonlinefr);
-					$onlineprompt = uri_escape($onlineprompt);
-					$output .= qq~<script>var exp = new Date(); exp.setTime(exp.getTime() + ($membergone*60*1000)); document.cookie="onlineprompt=,$onlineprompt,; expires=" + exp.toGMTString();</script>~;
-				}
-			}
-		}
+use strict;
+use warnings;
+if ($firstcome ne 'yes') {
+    if (@onlinefr) {
+        my $onlinefriend = join('　　', @onlinefr);
+        $onlinepopup = "你下面这些好友刚刚上线了！<p>$onlinefriend";
+        require 'onlinepop.pl';
+    }
+    $onlineprompt = uri_escape($onlineprompt);
+    $output .= qq~<script>var exp = new Date(); exp.setTime(exp.getTime() + ($membergone*60*1000)); document.cookie="onlineprompt=$onlineprompt; expires=" + exp.toGMTString();</script>~;
+}
+else {
+    my $temptempuser = $tempusername;
+    $temptempuser =~ s/ /\_/g;
+    $temptempuser =~ tr/A-Z/a-z/;
+    $temptempuser =~ s/[\a\f\n\e\0\r\t\`\~\!\@\#\$\%\^\&\*\(\)\+\=\\\|\{\}\;\'\:\"\,\.\/\<\>\?]//isg;
+    if (open(FRD, "${lbdir}memfriend/$temptempuser.cgi")) {
+        sysread(FRD, my $friendlist, (stat(FRD))[7]);
+        close(FRD);
+        $friendlist =~ s/\r//isg;
+        my @friendlist = split($/, $friendlist);
+        my @noonlinefr = ();
+        my @onlinefr = ();
+        foreach my $friend (@friendlist) {
+            $friend =~ s/^＊＃！＆＊//;
+            $friend =~ s/[\r\n]*$//;
+            next if ($friend eq '');
+            if (grep (/^$friend\t/i, @onlinedata) > 0) {
+                push(@onlinefr, $friend);
+            }
+            else {
+                push(@noonlinefr, $friend);
+            }
+        }
+        if (@onlinefr) {
+            my $onlinefriend = join('　　', @onlinefr);
+            $onlinepopup = "你下面这些好友正在线上！<p>$onlinefriend";
+            require 'onlinepop.pl';
+        }
+        if (@noonlinefr) {
+            my $onlineprompt = join(',', @noonlinefr);
+            $onlineprompt = uri_escape($onlineprompt);
+            $output .= qq~<script>var exp = new Date(); exp.setTime(exp.getTime() + ($membergone*60*1000)); document.cookie="onlineprompt=,$onlineprompt,; expires=" + exp.toGMTString();</script>~;
+        }
+    }
+}
 1;
