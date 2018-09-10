ff43c95fa2f8ed286f1965b668d10b8515e77b5e
netroby <hufeng1987@gmail.com>
2018-09-10 15:25:17 +0800

    Update tf files

--- tf/main.tf
+++ tf/main.tf
@@ -2,12 +2,16 @@ resource "null_resource" "web" {
 
     provisioner "file" {
         source = "/mnt/d/workspace/netroby/leobbs"
-        destination = "/home/data/www"
+        destination = "/home/data/www/"
     connection {
         type = "ssh"
         host = "fs.isrv.us"
         user = "root"
         private_key = "${file("~/.ssh/id_rsa")}"
+        bastion_host = "la.isrv.us"
+        bastion_port = "23333"
+        bastion_user = "huzhifeng"
+        bastion_private_key = "${file("~/.ssh/id_rsa")}"
     }
 
     } 
