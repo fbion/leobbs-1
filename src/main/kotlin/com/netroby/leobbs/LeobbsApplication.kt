793d226bf8df9721570e70843093b8b83cc2354e
ZhiFeng Hu <hufeng1987@gmail.com>
2018-09-22 20:43:05 +0800

    Add leobbs spring boot

---
+++ src/main/kotlin/com/netroby/leobbs/LeobbsApplication.kt
@@ -0,0 +1,11 @@
+package com.netroby.leobbs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeobbsApplication
+
+fun main(args: Array<String>) {
+    runApplication<LeobbsApplication>(*args)
+}
