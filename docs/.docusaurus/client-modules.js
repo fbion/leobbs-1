e09b0bdd7c755228557bdd096cc7f8a3b3b66929
zhifeng hu <hufeng1987@gmail.com>
2020-09-29 10:27:08 +0800

    移除不用的docs目录，文档已经迁移到新的独立仓库

--- docs/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("C:\\workspace\\leobbs\\leobbs\\docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\workspace\\leobbs\\leobbs\\docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\workspace\\leobbs\\leobbs\\docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\workspace\\leobbs\\leobbs\\docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\workspace\\leobbs\\leobbs\\docs\\node_modules\\@docusaurus\\theme-classic\\src\\prism-include-languages"),
-  require("C:\\workspace\\leobbs\\leobbs\\docs\\src\\css\\custom.css"),
-];
