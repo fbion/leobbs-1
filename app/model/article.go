d957a8dcb713ae8b73437af9993451651dc8b205
zhifeng hu <hufeng1987@gmail.com>
2020-11-02 09:25:44 +0800

    修改Article的model定义

--- app/model/article.go
+++ app/model/article.go
@@ -1,11 +1,16 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Article struct {
-	gorm.Model
-	Aid uint
+	ID        int64 `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 	Content string
 	Images string
-	PublishStatus uint;
+	PublishStatus int;
 }
