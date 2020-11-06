5740d163cb924d5fa8baebc5d30f6fceeebbe779
zhifeng hu <hufeng1987@gmail.com>
2020-11-06 08:45:43 +0800

    首页模板复原中。

--- app/controller/index_controller.go
+++ app/controller/index_controller.go
@@ -1,11 +1,21 @@
 package  controller
 
 import (
+	"gitee.com/leobbs/leobbs/app/skins"
 	"github.com/flosch/pongo2/v4"
 	"github.com/gin-gonic/gin"
 )
 
 func IndexAction(c *gin.Context) {
 
-	c.HTML(200, "index.html", pongo2.Context{"hello": "world"})
+	pongoContext := pongo2.Context{
+		"imagesurl": "/assets",
+		"skin":      "leobbs",
+		"hello":     "world",
+	}
+
+	for tmpKey, tmpV := range skins.GetLeobbsSkin() {
+		pongoContext[tmpKey] = tmpV
+	}
+	c.HTML(200, "index.html", pongoContext)
 }
