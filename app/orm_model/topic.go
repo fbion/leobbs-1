d9fa3c6bb92453c17b8a9093b0bddcc2d7362c9b
zhifeng hu <hufeng1987@gmail.com>
2020-11-17 09:46:29 +0800

    新增发帖的topic model数据结构

--- app/orm_model/topic.go
+++ app/orm_model/topic.go
@@ -1,1 +1,20 @@
 package orm_model
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Topic struct {
+
+	ID        int64 `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Title string
+	Content string
+	AuthorUid int64
+	PublishStatus int;
+	//TopicId 为0表示主贴，否则就是回复
+	TopicId int64;
+}
