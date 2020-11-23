5176ffc035bbae530fbe7ca7a10ac75ba6e84ba1
zhifeng hu <hufeng1987@gmail.com>
2020-11-23 08:39:00 +0800

    修改首页逻辑，获取登录态信息

--- app/controller/index_controller.go
+++ app/controller/index_controller.go
@@ -1,27 +1,17 @@
 package  controller
 
 import (
+	"gitee.com/leobbs/leobbs/app/service/account_service"
 	"gitee.com/leobbs/leobbs/app/service/forum_service"
 	"gitee.com/leobbs/leobbs/app/skins"
 	"gitee.com/leobbs/leobbs/app/vo"
 	"gitee.com/leobbs/leobbs/pkg/common"
 	"github.com/flosch/pongo2/v4"
-	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func IndexAction(c *gin.Context) {
-	currentMethod := "IndexAction"
-	safeSess := sessions.Default(c)
-	luUsername := safeSess.Get("lu_username")
-	common.Sugar.Infof(currentMethod +  " luUsername : %v", luUsername)
-	if luUsername == nil {
-		luUsername = ""
-	}
-	is_admin := safeSess.Get("is_admin")
-	if is_admin == nil {
-		is_admin = false
-	}
+	luUsername, luUid, is_admin := account_service.AuthGetLoginUinfo(c)
 
 	var forumOutList []vo.Forum_out_vo
 	forumList , err := forum_service.GetForumList()
@@ -45,6 +35,7 @@ func IndexAction(c *gin.Context) {
 		"skin":        "leobbs",
 		"hello":       "world",
 		"lu_username": luUsername,
+		"lu_uid": luUid,
 		"is_admin": is_admin,
 		"forumsList": forumOutList,
 	}
