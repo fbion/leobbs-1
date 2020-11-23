93fd02b43ccc0a600a469a7f3b1a60562238b12a
zhifeng hu <hufeng1987@gmail.com>
2020-11-23 08:44:02 +0800

    修改贴子页逻辑

--- app/controller/topic_controller/index.go
+++ app/controller/topic_controller/index.go
@@ -2,27 +2,19 @@ package topic_controller
 
 import (
 	"gitee.com/leobbs/leobbs/app/orm_model"
+	"gitee.com/leobbs/leobbs/app/service/account_service"
 	"gitee.com/leobbs/leobbs/app/skins"
 	"gitee.com/leobbs/leobbs/app/vo"
 	"gitee.com/leobbs/leobbs/pkg/common"
 	"github.com/flosch/pongo2/v4"
-	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func IndexAction(c *gin.Context) {
 
 	currentMethod := "IndexAction@topic_controller"
-	safeSess := sessions.Default(c)
-	luUsername := safeSess.Get("lu_username")
-	common.Sugar.Infof(currentMethod +  " luUsername : %v", luUsername)
-	if luUsername == nil {
-		luUsername = ""
-	}
-	is_admin := safeSess.Get("is_admin")
-	if is_admin == nil {
-		is_admin = false
-	}
+
+	luUsername, luUid, is_admin := account_service.AuthGetLoginUinfo(c)
 
 	var tmpPostList []vo.Post_out_vo
 
@@ -48,6 +40,7 @@ func IndexAction(c *gin.Context) {
 		"skin":        "leobbs",
 		"hello":       "world",
 		"lu_username": luUsername,
+		"lu_uid": luUid,
 		"is_admin": is_admin,
 		"postList": tmpPostList,
 	}
