fec255a2dc2c07460f33fb9f8f383761ee5e527e
zhifeng hu <hufeng1987@gmail.com>
2020-11-25 09:33:04 +0800

    完善贴子标题获取功能

--- app/controller/topic_controller/index.go
+++ app/controller/topic_controller/index.go
@@ -34,11 +34,48 @@ func IndexAction(c *gin.Context) {
 		return
 	}
 
+
+	var tmpTopic vo.Topic_out_vo
+
+	var tmpTopicRaw orm_model.Topic
+
+	result := common.DB.First(&tmpTopicRaw, id)
+	if result.Error != nil {
+		common.Sugar.Infof(currentMethod + " query Topic error: %v", result.Error)
+		common.ShowUMessage(c, &common.Umsg{
+			"贴子不存在",
+			"/",
+		})
+		return;
+	}
+	tmpTopic.ID = tmpTopicRaw.ID
+	tmpTopic.Title = tmpTopicRaw.Title
+	tmpTopic.ForumId = tmpTopicRaw.ForumId
+
+
+	var tmpForum vo.Forum_out_vo
+
+	var tmpForumRaw orm_model.Forum
+
+	result = common.DB.First(&tmpForumRaw, tmpTopic.ForumId)
+	if result.Error != nil {
+		common.Sugar.Infof(currentMethod + " query Forum error: %v", result.Error)
+		common.ShowUMessage(c, &common.Umsg{
+			"论坛不存在",
+			"/",
+		})
+		return;
+	}
+
+	tmpForum.ID = tmpForumRaw.ID
+	tmpForum.ForumName = tmpForumRaw.Name
+	tmpForum.ForumDesc = tmpForumRaw.Description
+
 	var tmpPostList []vo.Post_out_vo
 
 	var rawPostList []orm_model.Post
 
-	result := common.DB.Order("ID ASC").
+	result = common.DB.Order("ID ASC").
 		Where("topic_id = ? ", id).
 		Limit(20).
 		Offset(0).
@@ -54,6 +91,11 @@ func IndexAction(c *gin.Context) {
 			Content: v.Content,
 		})
 	}
+
+
+
+
+
 	pongoContext := pongo2.Context{
 		"imagesurl":   "/assets",
 		"skin":        "leobbs",
@@ -61,6 +103,8 @@ func IndexAction(c *gin.Context) {
 		"lu_username": luUsername,
 		"lu_uid": luUid,
 		"isAdmin": isAdmin,
+		"topic": tmpTopic,
+		"forum": tmpForum,
 		"postList": tmpPostList,
 	}
 
--- app/vo/topic_out_vo.go
+++ app/vo/topic_out_vo.go
@@ -3,4 +3,5 @@ package vo
 type Topic_out_vo struct {
 	ID int64
 	Title string
+	ForumId int64
 }
