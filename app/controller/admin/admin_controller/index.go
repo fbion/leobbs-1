5495829be3b23cda857b0bf8292eddbd800c6632
zhifeng hu <hufeng1987@gmail.com>
2020-12-02 09:10:10 +0800

    修改管理员首页模板

--- app/controller/admin/admin_controller/index.go
+++ app/controller/admin/admin_controller/index.go
@@ -1,13 +1,28 @@
 package admin_controller
 
 import (
+	"gitee.com/leobbs/leobbs/app/service/account_service"
+	"gitee.com/leobbs/leobbs/app/skins"
 	"github.com/flosch/pongo2/v4"
 	"github.com/gin-gonic/gin"
 )
 
 func Index(c *gin.Context) {
+	luUsername, luUid, isAdmin := account_service.AuthGetLoginUinfo(c)
 
-	c.HTML(200, "admin/index.html", pongo2.Context{"hello": "world"})
+	pongoContext := pongo2.Context{
+		"imagesurl":   "/assets",
+		"skin":        "leobbs",
+		"hello":       "world",
+		"lu_username": luUsername,
+		"lu_uid": luUid,
+		"isAdmin": isAdmin,
+	}
+
+	for tmpKey, tmpV := range skins.GetLeobbsSkin() {
+		pongoContext[tmpKey] = tmpV
+	}
+	c.HTML(200, "admin/index.html", pongoContext)
 }
 
 
